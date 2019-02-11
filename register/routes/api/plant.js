e6b498e06549a0815150018b0c5e9ef00450df41
Jean Fils-Aime <jeaneddy25@gmail.com>
2019-02-11 05:30:46 -0500

    er

--- register/routes/api/plant.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const plantController = require("../../controllers/plantController");
-
-
-router.route("/")
-  .post(plantController.create);
-
-
-module.exports = router;
